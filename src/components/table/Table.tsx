472d9f4503e6d5a583d54302f0594c3e1e6d5ae1
SHERRY699 <shehryarnasir89@gmail.com>
2025-02-28 21:33:25 +0500

    Table Added

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -6,7 +6,7 @@ const data = [
   { user: "Melas08", title: "Sweet Bonanza 1000", time: "21:20", bet: "2.00", multiplier: "x0.8", payout: "-1.60" },
   { user: "player_8b869d062b48", title: "Coin Up: Lightning", time: "21:20", bet: "15.00", multiplier: "x0", payout: "-15.00" },
   { user: "player_89b92504afb", title: "Lion Gems: Hold and Win", time: "21:20", bet: "4.00", multiplier: "x0", payout: "-4.00" },
-  { user: "player_01432947e776", title: "Sweet Dream Bonanza", time: "21:20", bet: "19.00", multiplier: "x0.2", payout: "-8.00" },
+  { user: "player_0143", title: "Sweet Dream Bonanza", time: "21:20", bet: "19.00", multiplier: "x0.2", payout: "-8.00" },
   { user: "Kyovet", title: "Royal Coins 2: Hold and Win", time: "21:20", bet: "2.00", multiplier: "x0.65", payout: "-0.70" },
 ];
 
